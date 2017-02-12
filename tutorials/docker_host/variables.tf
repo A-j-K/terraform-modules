68bd3ba54b234d4200a174ba4f0cb5eaa4a0bd7f
Andy Kirkham <andy.kirkham@logicnow.com>
2017-02-12 15:14:19 +0000

    Add iam_instance_profile do docker_host

--- tutorials/docker_host/variables.tf
+++ tutorials/docker_host/variables.tf
@@ -2,6 +2,7 @@
 variable "name" { default = "docker-host" }
 variable "ami"  { }
 variable "instance_type" { }
+variable "iam_instance_profile" { } 
 variable "subnet_id" { }
 variable "vpc_security_group_ids" { default = [] }
 variable "user_data" { }
